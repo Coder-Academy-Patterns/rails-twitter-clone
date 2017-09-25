efd7f13a4d2d7453651744a2af0885444c09a6fd
Matthew Lenton <matthew@lenton.id.au>
2017-09-25 17:58:40 +1000

    pointer comment for asset pipecline config in rails 5
    eg.  http://javascriptexamples.info/snippet/javascript/sprockets_manifest_directivesjs_tikitdo_javascript

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
+// here are the directories that rails 5 has configured
+// for the asset pipeline
+
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
